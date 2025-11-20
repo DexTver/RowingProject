465eae71230d2f9cab8cf975580f4d9dffb55166
ivans <ivan.sharapov.05@mail.ru>
2025-11-20 12:18:01 +0300

    (〃￣︶￣)人(￣︶￣〃)

--- target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"rowing.gwt.client"},{"l":"rowing.gwt.server"},{"l":"rowing.gwt.shared"}];updateSearchResults();
